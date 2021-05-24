dd957600e4190262702c48855ddc7db6065737d2
fdnando <fmurbra199@iesalandalus.com>
2021-05-24 12:31:35 +0200

    ejemplo de clase

--- 01.POO.py
+++ 01.POO.py
@@ -1,47 +1,98 @@
+
+
 class Ser_vivo():
 
+	# método mágico (constructor)
 
-	def __init__(self,vida,h2o,carbono):
+
+	def __init__(self,vida,edad):
 
 		self.vida=vida
-		self.h2o=h2o
-		self.carbono=carbono
-
+		self.edad=edad
 
 	def alimentacion(self):
-		print("Obtengo nutrientes para seguir con vida")
-	def respiracion(self):
-		print("Obtengo oxígeno para seguir con vida")
+		print("Obtengo nutrientes para seguir con vida")	
+
+
+	#método estático
+
+	@staticmethod
+	def respiracion():
+		print("Siempre respiro")
+
+	def cumplir_años(self):
+
+		print("Ha cumplido otro años más, ahora tiene " + str(self.edad+1))	
 
 
 
 
-class Mamíferos(Ser_vivo):
+class Mamiferos(Ser_vivo):
 
-	def __init__(self,extremidades,columna_vertebral):
+	def __init__(self,vida,edad,extremidades):
+
+
+		#hereda de Ser_vivo
+
+		Ser_vivo.__init__(self,vida,edad)
 
 		self.extremidades=extremidades
-		self.columna_vertebral=columna_vertebral
+		
 
 	def movilidad(self):
-		print("Puedo desplazarme")
+		print("Puedo desplazarme con mis " + str(self.extremidades) + " extremidades")
+
+
+	#sobre carga de operadores	
+
+	def __add__(self,otros):
+
+		total_extremidades= self.extremidades + otros.extremidades
+
+		return total_extremidades	
 
 
 
-class Persona(Mamíferos):
-
-	def pensar(self):
-		print("Hago uso de la razón")	
+class Persona(Mamiferos):
 
 
-niño=Persona(4,1)
+	def __init__(self,vida,edad,extremidades,pensar,identificador):
 
-print(niño.extremidades)
+		
+		Mamiferos.__init__(self,vida,edad,extremidades)
+
+		self.pensar=pensar
+
+		#visibilidad (privado entre comillas)
+
+		self.__identificador=identificador
 
 
 
 
+	def pensando(self):
 
+		if self.pensar==True:
+
+			print("Este humano piensa")
+
+		else:
+			
+			print("Este humano no piensa ")	
+
+
+
+	#método mágico
+
+	def __repr__(self):
+		return f"<identificador persona (DNI) : {self.__identificador}>"		
+
+
+#instancio clases			
+
+niño1=Persona(True,5,4,True,33)
+
+niño2=Persona(True,7,4,False,45)
 
 
 
